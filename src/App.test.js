6fea97f83a48255a31d82f7c6e2b924eefc2731c
murash4 <singleant@gmail.com>
2021-01-07 03:05:36 +0300

    add basic tests

--- src/App.test.js
+++ src/App.test.js
@@ -1,12 +1,29 @@
-import { configure } from 'enzyme'
-import { Adapter } from 'enzyme-adapter-react-16'
+import { configure, shallow } from 'enzyme'
+import Adapter from 'enzyme-adapter-react-16'
+import App from './App'
+import Character from './Character'
 
 configure({
 	adapter: new Adapter()
 })
 
 describe('App', () => {
-	it('Should render 3 characters in light side', () => {
+	let wrapper
 
+	beforeEach(() => {
+		wrapper = shallow(<App />)
+	})
+
+	it('Should render 3 characters in light side', () => {
+		expect(wrapper.find(Character)).toHaveLength(3)
+	})
+
+	it('Should render 2 characters in dark side', () => {
+		// wrapper = shallow(<App side={ 'dark' } />)
+		wrapper.setProps({
+			side: 'dark'
+		})
+
+		expect(wrapper.find(Character)).toHaveLength(2)
 	})
 })
